c4b46efe741f292797bbefc584c4ccf0d6f9b6f0
israelBuskila <israelad3@gmail.com>
2025-03-18 01:24:27 +0200

    add SVG Icons

---
+++ client/src/app/constants/icon.ts
@@ -0,0 +1,11 @@
+import BackIcon from '../../assets/icons/icontype=back.svg?react'
+import EditIcon from '../../assets/icons/icontype=edit.svg?react'
+import DeleteIcon from '../../assets/icons/icontype=delete.svg?react'
+import PlusIcon from '../../assets/icons/icontype=delete.svg?react'
+
+export const Icons = {
+    back: BackIcon,
+    edit: EditIcon,
+    delete: DeleteIcon,
+    plus: PlusIcon
+}
